43501e31f326a8eafdc3e5144f79c7fa86978662
EricEDX <yodenai-git@yahoo.fr>
2016-11-22 14:20:46 +0100

    sun shining

--- FichierEric.java
+++ FichierEric.java
@@ -4,6 +4,7 @@ public class FichierEric {
 
 	public static void main(String[] arts) {
 		System.out.println("Hello World");
+		System.out.println("Sun is shining.");
 		System.out.println("Eric.");
 	}
 
