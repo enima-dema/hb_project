d5a5d8b4e5fa42730023afff6002dc7990d8032e
taomaster <amine.meda@gmail.com>
2016-11-22 15:04:24 +0100

    Essaie Branche

--- test/TestAmine.java
+++ test/TestAmine.java
@@ -1,13 +1,12 @@
 package test;
 
-import com.sun.java_cup.internal.runtime.Scanner;
-
 public class TestAmine {
 
 	public static void main(String[] arts) {
 		System.out.println("Hello World");
 		System.out.println("The Number ONE");
 		System.out.println("Or maybethe number two");
+		System.out.println("Quand je suis dans ses bras, je vois la vie en rose");
 	}
 
 }
